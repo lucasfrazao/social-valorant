380e88ddf05303efb8cc5d3a539b05211dc8e50e
Lucas Frazao <lucasfraza0@outlook.com>
2022-05-10 04:10:04 -0300

    fix: settings for deploy

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
 import React, { ReactNode } from 'react'
 import { Container } from './styles'
 import { FaArrowLeft, FaBell } from 'react-icons/fa'
-import Button from '../Button'
 
 interface HeaderProps {
   children?: ReactNode
